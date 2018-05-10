83d421ed45b86ea10db7b14ae176155342314fac
andrewelamb <andrewelamb@gmail.com>
2018-05-10 11:15:41 -0700

    added new write yaml functions

--- utils/write_yaml.R
+++ utils/write_yaml.R
@@ -1,7 +1,6 @@
 require(yaml)
 require(purrr)
 
-
 create_kallisto_index_yaml <- function(
     yaml_file,
     fasta_file,
@@ -12,7 +11,29 @@ create_kallisto_index_yaml <- function(
         "index_file_string" = index_file_string)
     yaml::write_yaml(arg_list, yaml_file)
 }
+
+create_kallisto_quant_yaml <- function(
+    yaml_file, fastq1, fastq2, index,
+    threads = NULL){
     
+    arg_list = list(
+        "fastq1" = file_to_yaml_file(fastq1),
+        "fastq2" = file_to_yaml_file(fastq2),
+        "index" = file_to_yaml_file(index)
+    )
+    
+    option_list = 
+        list("threads" = seed) %>% 
+        purrr::discard(is.null)
+    
+    yaml::write_yaml(c(arg_list, option_list), yaml_file)
+}
+
+create_kallisto_h5dump_yaml <- function(h5){
+    arg_list = list("h5" = file_to_yaml_file(h5))
+    yaml::write_yaml(h5, yaml_file)
+}
+
 file_to_yaml_file <- function(file){
     list("path" = file, "class" = "File")
 }
